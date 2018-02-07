847a7799e22d30be49f703c83a1c8591cc04856d
lnoellaho <lnoellaho@gmail.com>
2018-02-07 11:54:08 -0800

    rename folder

--- surf-spot-frontend/src/Home.js
+++ surf-spot-frontend/src/Home.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
-import {ListGroup, ListGroupItem} from 'react-bootstrap'
-// import "./images/LandingPage.jpg";
+// import {ListGroup, ListGroupItem} from 'react-bootstrap'
+import "./App.css";
+
 
 export default class Home extends Component {
 
     render() {
         return (
             <div>
-                <header className="masthead">
-                    <h1>SurfSpot</h1>
+                <header className="masthead text-center text-white d-flex">
+                    <h1>SurfSpots</h1>
                 </header>
                 <div>
                 {this.props.beaches.map(function(element, key) {
-                  return  <a key={key} href={`/beaches/${element.id}`}><ListGroupItem key={key}>
+                  return  <a key={key} href={`/beaches/${element.id}`}><li key={key}>
                           <h2> {element.name} </h2>
-                          </ListGroupItem></a>
+                          </li></a>
                       })}
                 </div>
             </div>
