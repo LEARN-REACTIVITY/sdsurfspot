5461f5d64a0633f078f25dcac9312d525b4cdd20
lnoellaho <lnoellaho@gmail.com>
2018-02-07 10:27:28 -0800

    landing page

--- surf-spot frontend/src/home.js
+++ surf-spot frontend/src/home.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import {Card, CardTitle} from 'material-ui/Card';
+import {ListGroup, ListGroupItem} from 'react-bootstrap'
+// import "./images/LandingPage.jpg";
 
 export default class Home extends Component {
 
     render() {
         return (
             <div>
-            {this.props.beaches.map(function(element, key) {
-              return  <a key={key} href={`/beaches/${element.id}`}><Card key={key}>
-                      <CardTitle title={element.name} />
-                      </Card></a>
-                  })}
+                <header className="masthead">
+                    <h1>SurfSpot</h1>
+                </header>
+                <div>
+                {this.props.beaches.map(function(element, key) {
+                  return  <a key={key} href={`/beaches/${element.id}`}><ListGroupItem key={key}>
+                          <h2> {element.name} </h2>
+                          </ListGroupItem></a>
+                      })}
+                </div>
             </div>
         )
     }
