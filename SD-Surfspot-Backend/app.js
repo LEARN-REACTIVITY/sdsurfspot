38275de2219f8ad42ffbbccca77b99405b94c296
lnoellaho <lnoellaho@gmail.com>
2018-02-08 16:24:06 -0800

    form/relationships

--- SD-Surfspot-Backend/app.js
+++ SD-Surfspot-Backend/app.js
@@ -11,68 +11,27 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.json({message: 'API Example App'})
 })
-//
-// User.create({
-//     name:'Bob',
-//     userName: 'bobo123',
-//     email: 'iambob@gmail.com',
-//     friends: 1}).then(function(user){
-//         console.log(user.get);
-//         return user.createBeach({
-//             beach: 'cardiff',
-//             location: 'La Jolla'
-//         })
-//     })
-//     .then(function(beaches){
-//         console.log("favorite beach", beaches.get())
-//     }).catch(function(err){
-//         console.log(err)
-//     })
-//
-// });
 
-Beach.create({
-    name: 'cardiff',
-    location: 'la jolla'
-}).then(function(beach) {
-    console.log(beach);
-    return beach.createUser({
-        name: "bob",
-        userName: 'bobo123',
-        email: 'iambob@gmail.com',
-        friends: 1
+User.create({
+    name:'Bob',
+    userName: 'bobo123',
+    email: 'iambob@gmail.com',
+    friends: 1}).then(function(user){
+        console.log(user.get);
+        return user.createBeach({
+            beach: 'cardiff',
+            location: 'La Jolla'
+        })
     })
-}).then(function(user) {
-    console.log(success)
-}).catch(function(err) {
-    console.log(error)
-})
+    .then(function(beaches){
+        console.log("favorite beach", beaches.get())
+    }).catch(function(err){
+        console.log(err)
+    })
+
+});
+
 module.exports = app
-//
-
-
-
-
-
-
-
-
-// User.create({
-//     name:'Bob',
-//     userName: 'bobo123',
-//     email: 'iambob@gmail.com',
-//     friends: 1}).then(function(user){
-//         console.log(user.get);
-//         return user.createBeach({
-//             beach: 'cardiff',
-//             location: 'La Jolla'
-//         })
-//     })
-//     .then(function(beaches){
-//         console.log("favorite beach", beaches.get())
-//     }).catch(function(err){
-//         console.log(err)
-//     })
 
 
 /*
