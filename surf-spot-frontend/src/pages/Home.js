39e55bda7310790982c28678edd343c7cf4dd39b
lnoellaho <lnoellaho@gmail.com>
2018-02-08 17:16:06 -0800

    worked on homePage imgs and click button

--- surf-spot-frontend/src/pages/Home.js
+++ surf-spot-frontend/src/pages/Home.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-// import {ListGroup, ListGroupItem} from 'react-bootstrap'
+import {Row, Col, Grid, Image, Button} from 'react-bootstrap'
 
 
 export default class Home extends Component {
 
     render() {
         return (
-            <div>
-                <header className="masthead">
-                    <h1 className="mainheader">San Diego Surf Spots</h1>
-                    <h3 className="secondheader">Check out the daily local surf report!</h3>
-                </header>
-                <div>
+            <div className="locations">
+            <header className="masthead">
+            <h1 className="mainheader">San Diego Surf Spots</h1>
+            <h3 className="secondheader">Check out the daily local surf report!</h3>
+            </header>
                 {this.props.beaches.map(function(element, key) {
-                  return  <a key={key} href={`/beaches/${element.id}`}><li key={key}>
-                          <h2> {element.name} </h2>
-                          </li></a>
-                      })}
-                </div>
+                return  <div className= "box">
+                        <h4>{element.name}</h4>
+                            <a key={key}                            href={`/beaches/${element.id}`}>
+                                <Image className="locationImages" src={require("./default.jpg")} rounded />
+                                </a>
+                            <Button className="checkIn" bsSize="xsmall">Check In</Button>
+                        </div>
+                })}
             </div>
         )
     }
