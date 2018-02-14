492db48f956a771fca7d240f4921836de68da70e
christinamerz <christina.merz@hotmail.com>
2018-02-14 14:23:30 -0800

    frontend

--- surf-spot-frontend/src/pages/Home.js
+++ surf-spot-frontend/src/pages/Home.js
@@ -52,28 +52,23 @@ export default class Home extends Component {
     render() {
         return (
             <div className="locations" id="locations">
-            <header className="masthead">
-            </header>
-            <h3 id='spots' className="secondheader">CHECK OUT THE DAILY LOCAL SURF REPORT!</h3>
-                {this.props.beaches.map((element, key) => {
-                return  <div className= "box">
-                            <img src={require("../images/surfboard.jpg")} />
-                            <a key={key} href={`/beaches/${element.id}`}>
-                                <h4 className="locationNames">{element.name}</h4>
-                            </a>
+                    <header className="masthead">
+                    </header>
+                    <div id="spots" className="whiteboard">
+                    <h3 className="secondheader">CHECK OUT THE DAILY LOCAL SURF REPORT!</h3>
+                        {this.props.beaches.map((element, key) => {
+                        return  <div className= "box">
+                                    <a key={key} href={`/beaches/${element.id}`}>
+                                        <h4 className="locationNames">{element.name}</h4>
+                                    </a>
 
-                            <Button onClick={this.handleCheckIn.bind(this, element.name)} className="checkIn" bsSize="xsmall">Check In</Button>
-                            <p className="checkedIn">{this.state.checkedInCount} Surfers are checked in today</p>
+                                    <Button onClick={this.handleCheckIn.bind(this, element.name)} className="checkIn" bsSize="xsmall">Check In</Button>
+                                    <p className="checkedIn">{this.state.checkedInCount} Surfers are checked in today</p>
 
-         
-                        </div>
-                })}
 
-                <div>
-                  <h3 className='about'>About Our Staff</h3>
-                  <p>Christina, Leeann, Jordan, Max</p>
-                </div>
-
+                                </div>
+                        })}
+                    </div>
             </div>
         )
     }
