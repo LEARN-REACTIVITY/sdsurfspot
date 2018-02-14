2cf4991b904a5c46a22f00010c09387d055d453b
[max <maxbelonog@gmail.com>
2018-02-14 14:44:20 -0800

    user page 2

--- SD-Surfspot-Backend/index.js
+++ SD-Surfspot-Backend/index.js
@@ -1,5 +1,5 @@
 const app = require('./app')
 
-app.listen(3000, function () {
-    console.log('Todo Server listening on port 3000!');
+app.listen(3005, function () {
+    console.log('Todo Server listening on port 3005!');
 });
--- surf-spot-frontend/src/pages/UserMainPage.js
+++ surf-spot-frontend/src/pages/UserMainPage.js
@@ -1,43 +1,40 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar.js'; 
 import Footer from './Footer'; 
-import {Layout, Media} from 'react-bootstrap'
+import 'react-bootstrap';
+import Form from './Form';
 
-
-export default class UserMainPage extends Component {
-    render() {
+export default class UserMainPage extends Component { 
+ render(){   
+  return(  
+   function(getState) {
+        return({  
+            name:null,
+            userName:null,
+            email:null
+        })  
+    }
+    
+    componentDidMount() {
         return(
-            
-            <div className = 'userMainPage'>           
+            <div className = 'profile'>           
                 <main>
-                
-                    <div>
-
-                        <Media>
-                            <Media.Left align="top">
-                            <img width={64} height={64} src="/thumbnail.png" alt="thumbnail" />
-                            </Media.Left>
-                            <Media.Body>
-                            <Media.Heading>Top aligned media</Media.Heading>
-                            <p>
-                                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
-                                ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
-                                tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
-                                fringilla. Donec lacinia congue felis in faucibus.
-                            </p>
-
-                            <p>
-                                Donec sed odio dui. Nullam quis risus eget urna mollis ornare vel eu
-                                leo. Cum sociis natoque penatibus et magnis dis parturient montes,
-                                nascetur ridiculus mus.
-                            </p>
-                            </Media.Body>
-                        </Media>
-                       
-                    </div>
+                    <header rclassName='App-header'>
+                        <p>hi</p> 
+                    </header>    
+                    <body className = 'info'>
+                        <img src='/../public/imeges.profile.jpg' className="pic" alt="Users Profile pic goes here" />
+                        <h3>Name: {this.state.name}</h3>
+                        <h3>Email: {this.state.email}</h3>
+                        <h3>UserName: {this.state.username}</h3>
+                    </body>
                 </main>           
-            </div>
+            </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
         )
-    };
-
+    }
+  )   
+ }
 }
+
+export defualt UserMainPage;
+
