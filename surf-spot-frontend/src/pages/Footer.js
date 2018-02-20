1b1c6432715f1e1877e13a6cbfbb4456ea0de222
jcook1310 <jcook1310@gmail>
2018-02-20 10:18:43 -0800

    footer stuff :)

--- surf-spot-frontend/src/pages/Footer.js
+++ surf-spot-frontend/src/pages/Footer.js
@@ -4,11 +4,11 @@ export default class Footer extends Component {
 	render() {
 		return (
 			<div className="Footer" id="Footer">
-				<h2 className="TitleFooter">AKAW</h2>
+			<img className="logoNavBar" src={ require('../images/LogoNavBar.png') } />
                 <p className="TextFooter">
-                    reactivity@gmail.com<br/>
-                    &copy;<br/>
-                    API and swell information pulled from Spitcast and spitcast.com
+                    &copy; Reactivity 2018<br/>
+										reactivity@gmail.com<br/>
+                    Information pulled from Spitcast and spitcast.com
                 </p>
 			</div>
 		);
