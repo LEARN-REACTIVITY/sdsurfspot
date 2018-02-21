9ec2e81211e962ea9665916f52575f08c04574bc
Christina Merz <christinamerz@Christinas-MBP.localdomain>
2018-02-21 11:46:34 -0800

    indent

--- surf-spot-frontend/src/pages/About.js
+++ surf-spot-frontend/src/pages/About.js
@@ -8,8 +8,8 @@ render() {
 			<div id="About">
 			<div className="About">
 				<h2 className="TitleAbout text-uppercase">Welcome to AKAW<br/>San Diego's Surf Spot Locator!</h2>
-                <p className="TextAbout">Here you can check in and see the surf report and what's good at your favorite spot to avoid drop ins. Here at AKAW we allow surfers to check in to each location to show how crowded each spot is.
-                </p>
+        <p className="TextAbout">Here you can check in and see the surf report and what's good at your favorite spot to avoid drop ins. Here at AKAW we allow surfers to check in to each location to show how crowded each spot is.
+        </p>
 				<a id="signUpButton" className="btn btn-primary btn-lg text-uppercase js-scroll-trigger" href='/Form'>Sign Up Now</a>
 			</div>
 			</div>
